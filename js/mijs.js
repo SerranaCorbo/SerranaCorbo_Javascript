a204f5a437b60dbeaa1e4693ce9edaf741b78bae
SerranaCorbo <corboserrana@gmail.com>
2023-08-08 00:49:19 -0300

    cambios entrega final

--- js/mijs.js
+++ js/mijs.js
@@ -1,12 +1,19 @@
+//definicion de constantes y variables
+//definicion de los productos del carrito
 const productos = [
-    { nombre: "Base", precio: 600, marca: "Maybelline", imagen: "basemayb.webp" },
-    { nombre: "Rimel", precio: 800, marca: "Maybelline", imagen: "rimelmayb.jpeg " },
-    { nombre: "Delineador", precio: 1200, marca: "Mac", imagen: "delineadormac.jfif" },
-    { nombre: "Contorno", precio: 650, marca: "Maybelline", imagen: "contornomayb.jpg" },
-    { nombre: "Iluminador", precio: 650, marca: "Maybelline", imagen: "iluminadormayb.webp" },
-    { nombre: "Crema facial día", precio: 900, marca: "CeraVe", imagen: "cremadia.jpg" },
-    { nombre: "Crema de cuerpo", precio: 650, marca: "Dove", imagen: "cremacuerpo.jpg" },
-    { nombre: "Limpiador facial", precio: 750, marca: "CeraVe", imagen: "limpadorfacial.jpg" },
+    { nombre: "Base", precio: 600, marca: "Maybelline", categoria: "Maquillaje", imagen: "basemayb.webp" },
+    { nombre: "Rimel", precio: 800, marca: "Maybelline", categoria: "Maquillaje", imagen: "rimelmayb.jpg " },
+    { nombre: "Delineador", precio: 1200, marca: "Mac", categoria: "Maquillaje", imagen: "delineadormac.jpg" },
+    { nombre: "Contorno", precio: 650, marca: "Maybelline", categoria: "Maquillaje", imagen: "contornomayb.jpg" },
+    { nombre: "Iluminador", precio: 650, marca: "Maybelline", categoria: "Maquillaje", imagen: "iluminadormayb.webp" },
+    { nombre: "Crema facial día", precio: 900, marca: "CeraVe", categoria: "Cuidado facial y corporal", imagen: "cremadia.jpeg" },
+    { nombre: "Crema de cuerpo", precio: 650, marca: "Dove", categoria: "Cuidado facial y corporal", imagen: "cremacuerpo.jpg" },
+    { nombre: "Limpiador facial", precio: 750, marca: "CeraVe", categoria: "Cuidado facial y corporal", imagen: "limpadorfacial.jpg" },
+    { nombre: "Gloss", precio: 700, marca: "Maybelline", categoria: "Maquillaje", imagen: "glossmayb.jpg" },
+    { nombre: "Labial", precio: 750, marca: "Maybelline", categoria: "Maquillaje", imagen: "labialmayb.jpg" },
+    { nombre: "Labial mate", precio: 790, marca: "Revlon", categoria: "Maquillaje", imagen: "labialrev.webp" },
+    { nombre: "Polvo compácto", precio: 490, marca: "Revlon", categoria: "Maquillaje", imagen: "polvorev.webp" },
+
 ];
 
 let nombreItem;
@@ -20,24 +27,61 @@ let continuar;
 let agregarProducto;
 let carrito = [];
 
-// guardo el carrito en el localStorage
+//funciones relacionadas al carrito y amacenamiento
 function carritoAlStorage() {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
-
-// traer el carrito del localStorage
 function traerCarrito() {
     const carritoGuardado = localStorage.getItem("carrito");
     carrito = carritoGuardado ? JSON.parse(carritoGuardado) : [];
 }
 
-// guardar el carrito en el localStorage
+//carga inicial del carrito
 carritoAlStorage();
-
-// traer el carrito del localStorage
 traerCarrito();
 
-// creo tabla
+
+//funciones relacionadas a la interfaz
+//funcion para mostrar el catalogo
+function mostrarCatalogo() {
+    let seccionCatalogo = document.getElementById('idCatalogo');
+    seccionCatalogo.innerHTML = '';
+    productos.forEach((producto) => {
+        tarjeta(producto, seccionCatalogo);
+    });
+}
+
+//creo tarjetas del catalogo
+function tarjeta(producto, seccion) {
+    let { nombre, precio, imagen } = producto;
+    let card = document.createElement('div');
+    card.classList.add('d-flex', 'justify-content-center', "cardCatalogo");
+
+    let figure = document.createElement('figure');
+    figure.classList.add('producto');
+    figure.innerHTML = `<div class="imgprod"><img src="./img/${producto.imagen}"></div>
+                        <div class="descripcionprod">
+                            <figcaption><p class="titulocard">${producto.nombre} - ${producto.marca} </p></figcaption>
+                            <p class="textoprecio">precio: $${producto.precio.toLocaleString()} c/u</p>
+                            <button class= "botonProductos btn btn-light" id="agregarcarrito-${producto.nombre}">Agregar al carrito</button>
+                        </div>`;
+    card.appendChild(figure);
+    seccion.appendChild(card);
+    const botonAgregar = document.getElementById(`agregarcarrito-${nombre}`);
+    botonAgregar.onclick = () => { 
+        agregarCarrito(producto) 
+        Toastify({
+            text: "¡Producto agregado al carrito!",   
+            gravity: 'bottom',        
+            duration: 2000,
+            style: {
+                background: "#c98e8e",
+            },           
+            }).showToast();
+    };
+}
+
+//creo tabla
 const tabla = document.createElement('table');
 tabla.classList.add('table', 'table-light');
 
@@ -66,51 +110,51 @@ thead.appendChild(tr);
 tabla.appendChild(thead);
 
 const tbody = document.createElement('tbody');
+tbody.classList.add('tablabody');
 tbody.setAttribute('id', 'tablaBody');
 tabla.appendChild(tbody);
 
-// agrego tabla a mi section en html
+//agrego tabla a mi section en html
 const tablaCarrito = document.getElementById('tablaCarrito');
 tablaCarrito.appendChild(tabla);
+tablaCarrito.style.display = 'none';
 
-// catalogo y cards
+//creo botones de limpiar y comprar
+const botonLimpiar = document.createElement('button');
+botonLimpiar.className = 'botonLimpiar btn btn-light';
+botonLimpiar.innerText = 'Limpiar todo';
+botonLimpiar.addEventListener('click', () => { limpiarCarrito() });
 
-function mostrarCatalogo() {
-    let seccionCatalogo = document.getElementById('idCatalogo');
-    seccionCatalogo.innerHTML = '';
-    productos.forEach((producto) => {
-        tarjeta(producto, seccionCatalogo);
-    });
-}
+const botonComprar = document.createElement('button');
+botonComprar.className = 'botonComprar btn btn-light';
+botonComprar.innerText = 'Realizar compra';
+botonComprar.addEventListener('click', () => { realizarCompra() });
 
-function tarjeta(producto, seccion) {
-    let { nombre, precio, imagen } = producto;
-    let card = document.createElement('div');
-    card.classList.add('d-flex', 'justify-content-center', "cardCatalogo");
+//agrego botones al contenedor
+const contenedorBotones = document.createElement('div')
+contenedorBotones.classList.add('d-flex', 'justify-content-end');
+contenedorBotones.appendChild(botonLimpiar);
+contenedorBotones.appendChild(botonComprar);
 
-    let figure = document.createElement('figure');
-    figure.classList.add('producto');
-    figure.innerHTML = `<div class="imgprod"><img src="./img/${producto.imagen}"></div>
-                        <div class="descripcionprod">
-                            <figcaption><p>${producto.nombre} - ${producto.marca} </p></figcaption>
-                            <p>precio: $${producto.precio.toLocaleString()}</p>
-                            <button class= "btn btn-light" id="agregarcarrito-${producto.nombre}">+ Añadir al carrito</button>
-                        </div>`;
-    card.appendChild(figure);
-    seccion.appendChild(card);
-    const botonAgregar = document.getElementById(`agregarcarrito-${nombre}`);
-    botonAgregar.onclick = () => { 
-        agregarCarrito(producto) 
-        Toastify({
-            text: "¡Producto agregado al carrito!",           
-            duration: 2000,
-            style: {
-                background: "linear-gradient(to right, #FFB5B6, #DE8789)",
-            },           
-            }).showToast();
-    };
-}
+//agrego el contenedor al final de la tablaCarrito
+tablaCarrito.appendChild(contenedorBotones);
 
+const botonVerCarrito = document.getElementById('botonVerCarrito');
+const rect = botonVerCarrito.getBoundingClientRect();
+tablaCarrito.style.top = rect.bottom + 'px';
+tablaCarrito.style.left = rect.left + 'px';
+botonVerCarrito.addEventListener('click', () => {
+    if (tablaCarrito.style.display === 'none') {
+        tablaCarrito.style.display = 'block';
+        tablaCarrito.style.top = rect.bottom + 'px';
+        tablaCarrito.style.left = rect.right - tablaCarrito.offsetWidth + 'px';
+        mostrarCarrito();
+    } else {
+        tablaCarrito.style.display = 'none';
+    }
+});
+
+//funcion para mostrar el contenido del carrito
 function mostrarCarrito() {
     let tablaBody = document.getElementById('tablaBody');
     tablaBody.innerHTML = '';
@@ -131,9 +175,9 @@ function mostrarCarrito() {
         dato.classList.add('tdCantidad');
         dato.innerHTML = `
             <div class="celdaCantidad">
-                <button class= "btn btn-light" id="restar-${producto.nombre}">-</button>
+                <button class= "botonRestar btn btn-light" id="restar-${producto.nombre}">-</button>
                 <p class="cantidadTicket">${producto.cantidad}</p>
-                <button class= "btn btn-light" id="sumar-${producto.nombre}">+</button>
+                <button class= "botonSumar btn btn-light" id="sumar-${producto.nombre}">+</button>
             </div>
         `;
         fila.appendChild(dato);
@@ -167,14 +211,26 @@ function mostrarCarrito() {
     filaTotal.appendChild(tdTotal);
 
     tablaBody.appendChild(filaTotal);
-
-    const botonLimpiar = document.createElement('button');
-    botonLimpiar.className = 'limpiar Carrito btn btn-light';
-    botonLimpiar.innerText = 'Limpiar todo';
-    tablaBody.appendChild(botonLimpiar);
-    botonLimpiar.addEventListener('click', () => { limpiarCarrito() });
 }
 
+//funcion para filtrar los productos por marca
+function filtrarPorMarca() {
+    const filtroMarca = document.getElementById('filtroMarca').value;
+
+    let seccionCatalogo = document.getElementById('idCatalogo');
+    seccionCatalogo.innerHTML = '';
+
+    productos.forEach((producto) => {
+        if (filtroMarca === 'todos' || producto.marca === filtroMarca) {
+            tarjeta(producto, seccionCatalogo);
+        }
+    });
+}
+
+const selectFiltroMarca = document.getElementById('filtroMarca');
+selectFiltroMarca.addEventListener('change', filtrarPorMarca);
+
+//funcion para agregar productos al carrito
 function agregarCarrito(eleccion) {
         let productoEnCarrito = carrito.find((producto) => producto.nombre === eleccion.nombre);
         if (productoEnCarrito) {
@@ -187,25 +243,31 @@ function agregarCarrito(eleccion) {
         mostrarCarrito();
     }
 
-    function restarProducto(eleccion){
-        let indice = carrito.findIndex((el) => el.id === eleccion.id);
-        if (carrito[indice].cantidad == 1){
-            carrito.splice([indice],1);
-        }else{
-            carrito[indice].cantidad--;
+//funcion para restar productos del carrito    
+function restarProducto(eleccion) {
+    let productoEnCarrito = carrito.find((producto) => producto.nombre === eleccion.nombre);
+    if (productoEnCarrito) {
+        if (productoEnCarrito.cantidad === 1) {
+            carrito = carrito.filter((producto) => producto.nombre !== eleccion.nombre);
+            } else {
+            productoEnCarrito.cantidad--;
         }
         carritoAlStorage();
         mostrarCarrito();
     }
-
-    function calcularTotalCarrito() {
+}
+    
+//funcion para calcular el total del carrito
+function calcularTotalCarrito() {
         return carrito.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0);
-    }
+}
 
-    function limpiarCarrito() {
+//funcion para limpiar el carrito
+function limpiarCarrito() {
         Swal.fire({
-            //title: '¿Estás seguro?',
+            title: '¿Estás seguro?',
             text: '¿Deseas limpiar todos los items del carrito?',
+            icon: 'warning',
             showCancelButton: true,
             confirmButtonText: 'Sí, limpiar carrito',
             confirmButtonColor: '#ED9192',
@@ -227,45 +289,37 @@ function agregarCarrito(eleccion) {
                 });
                 }
             });
-    }
-    
-mostrarCatalogo();
+}
 
-
-
-
-
-
-
-
-
-
-/*        carrito.splice(0, carrito.length);
-carrito = [];
-carritoAlStorage();
-mostrarCarrito();/*/
-
-///loggeo para que el ususario se loggee
-
-/*const BOTONLOGIN = document.getElementById("otroBoton");
-const USUARIO = 
-
-BOTONLOGIN.addEventListener('click', () => {
-    Swal.fire({
-        title: "Login de usuario",
-        text: "Bienvenido al login de Pepes Page",
-        inputLabel: "Ingrese su nombre de usuario",
-        inputPlaceholder: "pepe@pepe.com", // Corrección aquí: inputPlaceholder en lugar de inputPlaceHolder
-        input: "email",
-        confirmButtonText: "Enviar",
+//función para simular la realizacion de la compra
+function realizarCompra() {
+        Swal.fire({
+        title: "¡Ya casi está listo!",
+        text: '¿Desea continuar con su compra?',
         showCancelButton: true,
-        cancelButtonText: "cancelar",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            const usuario = result.value;
-            // Aquí puedes realizar las acciones con el nombre de usuario ingresado, por ejemplo, enviarlo al servidor para autenticación.
-            // También puedes utilizar SweetAlert para mostrar mensajes de éxito o error en caso de que el login sea exitoso o falle.
-        }
-    });
-});
-*/
+        confirmButtonColor: '#ED9192',
+        confirmButtonText: 'Ir a pagar',
+        cancelButtonText: 'Aún no',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                carrito.splice(0, carrito.length);
+                carrito = [];
+                carritoAlStorage();
+                mostrarCarrito();
+                console.log("el usuario realizo una compra")
+                Swal.fire({
+                    title: '¡Compra realizada!',
+                    text: 'Su pago fue efectuado con éxito.',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar',
+                    confirmButtonColor: '#ED9192',
+                });
+            }
+        });
+}
+    
+// inicialización
+function inicializar() {
+    mostrarCatalogo();
+}    
+inicializar();
